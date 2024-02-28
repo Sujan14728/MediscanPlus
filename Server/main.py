71a3c33f7d5604e46666e5bdd353af0975111c86
YashwanKafle <077bct093@ioepc.edu.np>
2024-02-28 19:05:34 +0545

    refactor: formated main.py

--- Server/main.py
+++ Server/main.py
@@ -1,57 +1,57 @@
-from fastapi import FastAPI,Path
+from fastapi import FastAPI, Path
 from pydantic import BaseModel
 
+
 class Student(BaseModel):
-    name:str
-    age:int
-    year:str
+    name: str
+    age: int
+    year: str
+
 
 class UpdateStudent(BaseModel):
-    name:str |None=None
-    age:int |None=None
-    year:str |None=None
+    name: str | None = None
+    age: int | None = None
+    year: str | None = None
+
 
 app = FastAPI()
 
 
 students = {
-    1: {
-        "name": "John",
-        "age": 17,
-        "year": "year 12"
-    },
+    1: {"name": "John", "age": 17, "year": "year 12"},
 }
+
 
 @app.get("/")
 async def index():
-    return {"name":"initial data"}
+    return {"name": "initial data"}
+
 
 @app.get("/get-students/{student_id}")
-async def get_student(student_id:int):
+async def get_student(student_id: int):
     return students[student_id]
+
 
 @app.get("/get-by-name")
-async def get_student(name:str | None=None):
+async def get_student(name: str | None = None):
     for student_id in students:
-        if students[student_id]["name"] ==name:
+        if students[student_id]["name"] == name:
             return students[student_id]
-    return {"Data":"Not found"}
-    
+    return {"Data": "Not found"}
+
 
 @app.post("/create-student/{student_id}")
-async def create_student(student_id:int,student:Student):
+async def create_student(student_id: int, student: Student):
     if student_id in students:
-        return {"Error":"student already exists"}
-    
+        return {"Error": "student already exists"}
+
     students[student_id] = student
     return students[student_id]
+
 
 @app.put("/update-student/{student_id}")
-async def updata_student(student_id:int,student:UpdateStudent):
+async def updata_student(student_id: int, student: UpdateStudent):
     if student_id not in students:
-        return {"Error":"Student doesnot exists"}
+        return {"Error": "Student doesnot exists"}
     students[student_id] = student
     return students[student_id]
-
-
-
